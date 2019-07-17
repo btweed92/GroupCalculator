e6477d5fab2438629ffc75ad36e1fec3810c9839
Brandyn Tweedly <tweedly92@gmail.com>
2019-07-17 15:30:11 -0400

    first commit

---
+++ GroupCalculator/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            //prompt
+            bool go = true;
+            while (go)
+            {
+                PrintMenu();
+
+                //receive
+                string userChoice = Console.ReadLine();
+                
+
+                //processing
+                switch (userChoice.ToLower())
+                {
+                    case "1":
+                    case "addition":
+                        //input, getting numbers from user
+                        double num1 = ReadDoubles("Number 1: ");
+                        double num2 = ReadDoubles("Number 2: ");
+                        //processing
+                        double sum = AddDoubles(num1, num2);
+                        //output
+                        Console.WriteLine($"{num1} + {num2} = {sum}");
+                        break;
+
+                    case "2":
+                    case "subtraction":
+                        //input, getting numbers from user
+                        double num3 = ReadDoubles("Number 1: ");
+                        double num4 = ReadDoubles("Number 2: ");
+                        //processing
+                        double rem = SubDoubles(num3, num4);
+                        //output
+                        Console.WriteLine($"{num3} - {num4} = {rem}");
+                        break;
+
+                    case "3":
+                    case "mulitplication":
+                        //input, getting numbers from user
+                        double num5 = ReadDoubles("Number 1: ");
+                        double num6 = ReadDoubles("Number 2: ");
+                        //processing
+                        double prod = MultiDoubles(num5, num6);
+                        //output
+                        Console.WriteLine($"{num5} * {num6} = {prod}");
+                        break;
+
+                    case "4":
+                    case "division":
+                        //input, getting numbers from user
+                        double num7 = ReadDoubles("Number 1: ");
+                        double num8 = ReadDoubles("Number 2: ");
+                        //processing
+                        double quot = DivDoubles(num7, num8);
+                        //output
+                        Console.WriteLine($"{num7} / {num8} = {quot}");
+                        break;
+
+                    case "5":
+                    case "exit":
+                        go = false;
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+            //output
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Exit");
+        }
+        public static double ReadDoubles(string message)
+        {
+            //input
+            //prompt
+            Console.WriteLine(message);
+            //retrieve
+            string input = Console.ReadLine();
+            //processing
+            double num = double.Parse(input);
+            //output
+            return num;
+        }
+
+        public static double AddDoubles(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double SubDoubles(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static double MultiDoubles(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double DivDoubles(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ GroupCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GroupCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("GroupCalculator")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71f7ccad-d68f-4024-9227-a53b44fa03fa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
